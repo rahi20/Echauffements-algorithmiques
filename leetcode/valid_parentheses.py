5bfe4c27e07a1495448db6d451c1e124a67971e0
Rahima Largo <61783576+rahi20@users.noreply.github.com>
2021-10-25 13:15:35 +0100

    Valid Parentheses Prob

---
+++ leetcode/valid_parentheses.py
@@ -0,0 +1,14 @@
+#Easy Prob 20: Valid Parentheses
+#s contains only parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        n = len(s)
+
+        #save the paran char and it's index
+        op_par = {}
+
+        for i in range(n):
+            
+        return True
